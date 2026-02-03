5193169ad05556003b383a25d02adec3bacfea85
GClishe <clishegavin@gmail.com>
2026-02-02 20:34:21 -0500

    renamed folder

--- Gavin_Implementaiton/Clishe_SA.py
+++
@@ -1,7 +0,0 @@
-from Place_Benchmarks.Place_25 import data
-
-print(data)
-
-import os, sys
-print("cwd:", os.getcwd())
-print("sys.path[0]:", sys.path[0])
---
+++ Gavin_Implementation/Clishe_SA.py
@@ -0,0 +1,3 @@
+from Gavin_Implementation.Place_Benchmarks.Place_25 import data
+
+print(data)
