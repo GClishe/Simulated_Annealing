b292c79c195e1ffac0dd42ce40d1e683f2545f5e
GClishe <clishegavin@gmail.com>
2026-02-15 11:55:56 -0500

    added simulation results for place benchmarks

--- Clishe_Lowry/RunAll1Hour.sh
+++ Clishe_Lowry/RunAll1Hour.sh
@@ -1,39 +1,39 @@
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_25.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_25.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_50.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_50.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_100.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_100.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_500.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_500.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_1000.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_1000.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_5000.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_5000.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_10000.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_10000.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_15000.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_15000.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_20000.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_20000.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_25000.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_25000.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_25.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_25.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_50.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_50.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_100.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_100.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_500.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_500.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_1000.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_1000.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_5000.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_5000.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_10000.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_10000.txt
-sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_15000.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_15000.txt
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_25000.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_25000.txt
 sleep 30
 /usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_20000.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_20000.txt
 sleep 30
-/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_25000.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_25000.txt
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_15000.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_15000.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_10000.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_10000.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_5000.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_5000.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_1000.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_1000.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_500.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_500.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_100.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_100.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_50.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_50.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Place_25.py 2>&1 | tee Clishe_Lowry_Place_Benchmarks/Place_25.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_25000.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_25000.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_20000.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_20000.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_15000.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_15000.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_10000.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_10000.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_5000.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_5000.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_1000.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_1000.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_500.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_500.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_100.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_100.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_50.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_50.txt
+sleep 30
+/usr/bin/time -v python3 ./Clishe_Lowry_SA_Ptest_25.py 2>&1 | tee Clishe_Lowry_Ptest_Tests/Ptest_25.txt
